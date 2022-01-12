9e8a90b94f0955d2d849d095a88c9d39f2e1de54
vcici <57315480+vcici@users.noreply.github.com>
2022-01-12 21:19:23 +0200

    trecia pamoka

--- files.js
+++ files.js
@@ -1,1 +1,32 @@
-console.log('Labas');
+console.clear();
+
+const fs = require('fs/promises');
+
+
+(async () => {
+    const drinksContent = await fs.readFile('./data/drinks.txt', 'utf-8');
+    const drinksList = drinksContent.split('\r\n');
+    console.log(drinksContent);
+    console.log(drinksList);
+    
+
+
+    let i = 0;
+    for(const drink of drinksList){
+        // const drinkDetails = drink.split(' ');
+        // const drinkName = drinkDetails[0];
+        // const drinkPrice = drinkDetails[1];
+
+        const drinkDetails = drink.split(' ');      
+        const [drinkName, drinkPrice, count] = drinkDetails;
+        const apyvarta = parseInt(count) * parseFloat(drinkPrice);
+        // const apyvarta = +count * +drinkPrice; pliusai pavers i skaicius
+        console.log(`Pasirinkimas ${++i}: ${drinkName} kurio kaina yra ${drinkPrice} euru. Buvo parduota ${count} kiekis ir bendra apyvarta yra ${apyvarta.toFixed(2)} EUR`);
+    }
+    console.log('Viso gerimu:', i);
+
+    //Pasirinkimas1: [gerimo-pavadinimas] kurio kaina yra [kaina] euru.
+    //Pasirinkimas2: [gerimo-pavadinimas] kurio kaina yra [kaina] euru.
+    //Pasirinkimas3: [gerimo-pavadinimas] kurio kaina yra [kaina] euru.
+
+})();
